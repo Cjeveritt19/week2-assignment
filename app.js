7ffceb70b782ca384450423b5a9d8eb036f84aa5
Cjeveritt19 <callumeveritt11@gmail.com>
2025-08-22 11:25:33 +0100

    added image data  and css reset

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("hello world");
+
+
+//TODO: I need to store my image data
+//Either use immages stored locally or remotely
+const images =[
+    {
+        url: "url1", //relative path for local images OR link to image
+        altText:"altText1",
+    },
+    {
+        url: "url2",
+        altText: "altText2",
+    },
+    {
+        url: "url3",
+        altText: "altText3",
+    }
+]
+
+//TODO: I need to create my thumbnail images 
+
+//TODO: I need to create my large images
