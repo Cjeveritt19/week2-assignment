e023fdf8c3a624c5e1bb1516e58c0c74a016633a
Cjeveritt19 <callumeveritt11@gmail.com>
2025-08-23 11:26:32 +0100

    finished full funtion for  the thumbnails

--- app.js
+++ app.js
@@ -20,14 +20,23 @@ const images =[
 
 //TODO: I need to create my thumbnail images 
 function createThumbnails(){
-//Select the DOM element (thumbnail-container) to contain our thumbnails
-    const thumbnail = document.getElementById("thumbnail-container")
-    images.forEach(function (thumbnails){ 
-    const img1 = document.createElement("img1");
-    img1.src= thumbnails.url;
-    img1.alt= thumbnails.altText;
-    img1.className = "Animals"
-})
+//\Select the DOM element (thumbnail-container) to contain our thumbnails
+    const thumbnail = document.getElementById("thumbnail-container");
+    for (let i = 0; i < images.length; i++) {
+    // images.forEach(function (thumbnails){ 
+    const img1= document.createElement("img");
+    img1.src= images[i].url;
+    img1.alt= images[i].altText;
+    img1.className= "Animals";
+    img1.addEventListener("click",function () {
+        // createLargeImagesHandler(i);
+        console.log (img1)
+    });
+    thumbnail.appendChild(img1);
+
+}}
+
+    createThumbnails ();
 
 
 
@@ -37,15 +46,14 @@ function createThumbnails(){
 
 
 
-
-// This is a repetitive task loop through our array using the length property
-// Inside the loop I need to this:
-//- create the img element
-//- update the src and alt attributes of the img element to match those in the array(parameters)
-//- give each img a className(img.className)
+// \This is a repetitive task loop through our array using the length property
+// \Inside the loop I need to this:
+//- \create the img element
+//- \update the src and alt attributes of the img element to match those in the array(parameters)
+//- \give each img a className(img.className)
 //- add an event to each image event handler of this listner is the function you write to create large images 
 //- append the created images to the thumbnail-container
-}
+
 //TODO: I need to create my large images
 //This task is the event handler for the thumbnail events 
 function createLargeImagesHandler(){
@@ -62,4 +70,4 @@ function createLargeImagesHandler(){
 }
 
 //Add this event handler to the thumbnail event
-//Make sure you call the createThumbnails function
+//Make sure you call the createThumbnails function//
